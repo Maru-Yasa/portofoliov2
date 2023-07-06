7becae13edf0a370390c57a1a809e06ccda74fb9
Maru-Yasa <marufilyasa13@gmail.com>
2023-07-06 08:05:52 +0700

    add meta data

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,26 @@ import React from "react";
 import App from "./App";
 import "./index.css";
 import { ParallaxContext, ParallaxProvider } from "react-scroll-parallax";
+import DocumentMeta from "react-document-meta";
+
+const meta = {
+    title: 'MaruYasa Portofolio',
+    description: 'Fullstack Web Developer Enthusiast | PHP, NodeJS, React',
+    meta: {
+      charset: 'utf-8',
+      name: {
+        keywords: 'maruyasa,maru,yasa,portofolio'
+      }
+    }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <ParallaxProvider>
-      <App />
-    </ParallaxProvider>
+    <DocumentMeta {...meta}>
+      <ParallaxProvider>
+        <App />
+      </ParallaxProvider>
+    </DocumentMeta>
   </React.StrictMode>,
   document.getElementById("root")
 );
